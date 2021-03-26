e4a93e37d485c814dbe8838a3f113d1c92f5f526
Anton Voskanyan <voskanvl@gmail.com>
2021-03-26 18:27:57 +0300

    change digitButtons

--- src/components/digitButton.jsx
+++ src/components/digitButton.jsx
@@ -1,11 +1,26 @@
-import styles from "./digitButton.module.scss"
+/** @format */
 
-const DigitButton =({color,label,cb})=>(
-    <div onClick={()=>cb(label)} className={styles.button}>
-        <div style={{backgroundColor:color}}>
-            <div style={{color:'gray'}}>{label}</div>
-        </div>
-    </div>
-)
+import styles from "./digitButton.module.scss";
+const colors = [
+    { value: "red", text: "black" },
+    { value: "black", text: "white" },
+    { value: "white", text: "black" },
+    { value: "blue", text: "red" },
+];
 
-export default DigitButton
+const DigitButton = ({ color, label, cb }) => {
+    if (colors.map(color => color.value).includes(color)) {
+        const text = colors.filter(e => e.value === color)[0];
+        return (
+            <div onClick={() => cb(label)} className={styles.button}>
+                <div style={{ backgroundColor: color }}>
+                    <div style={{ color: text.text }}>{label}</div>
+                </div>
+            </div>
+        );
+    } else {
+        return <div>Не установленный цвет</div>;
+    }
+};
+
+export default DigitButton;
